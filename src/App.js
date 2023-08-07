38ae89728b9d08e8a5248bf0a8c3092909576cf7
BITTU KUMAR <dev.bittu96@gmail.com>
2023-08-07 12:30:40 +0530

    Add navbar

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="">
+      <nav className="navbar navbar-light bg-light">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img
+              src="/docs/5.0/assets/brand/bootstrap-logo.svg"
+              alt=""
+              width="30"
+              height="24"
+            />
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
